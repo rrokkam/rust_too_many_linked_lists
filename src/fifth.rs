532441f228d5332dec87a92c1067328ed9a77211
rrokkam <rohithrokkam@gmail.com>
2021-06-18 13:23:11 -0700

    Unsafe Basics

--- src/fifth.rs
+++ src/fifth.rs
@@ -1,7 +1,6 @@
-#[derive(Default)]
-pub struct List<'a, T> {
+pub struct List<T> {
     head: Link<T>,
-    tail: Option<&'a mut Node<T>>,
+    tail: *mut Node<T>,
 }
 
 type Link<T> = Option<Box<Node<T>>>;
@@ -11,38 +10,46 @@ struct Node<T> {
     next: Link<T>,
 }
 
-impl<'a, T> List<'a, T> {
+impl<T> List<T> {
     pub fn new() -> Self {
         List {
             head: None,
-            tail: None,
+            tail: std::ptr::null_mut(),
         }
     }
 
-    pub fn push(&'a mut self, elem: T) {
-        let new_tail = Box::new(Node { elem, next: None });
-        let new_tail = match self.tail.take() {
-            Some(old_tail) => {
-                old_tail.next = Some(new_tail);
-                old_tail.next.as_deref_mut()
+    pub fn push(&mut self, elem: T) {
+        let mut new_tail = Box::new(Node { elem, next: None });
+        let raw_tail: *mut _ = &mut *new_tail;
+        if !self.tail.is_null() {
+            // old tail existed -- update it to point to the new tail
+            // this is the "inverted push"
+            unsafe {
+                (*self.tail).next = Some(new_tail);
             }
-            None => {
-                self.head = Some(new_tail);
-                self.head.as_deref_mut()
-            }
-        };
-        self.tail = new_tail;
+        } else {
+            // this is the first elem in the list.
+            // update head to point to this as the new tail
+            self.head = Some(new_tail);
+        }
+        self.tail = raw_tail;
     }
 
-    pub fn pop(&'a mut self) -> Option<T> {
+    pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|head| {
             // this head node will be dropped
             let head = *head;
             self.head = head.next;
 
             // emptying out the list
+            // if we don't do this here, we won't see any problems.
+            // but the next calls to push will write to a dangling tail..
+            // Safety/unsafety can depend on state established outside a block!!!
+            // this is "unsafe taint".. limit it by watching the module's public APIs
+            // if no one externally can screw things up by any combinations of calls
+            // to those APIs, then from an external perspective this is safe.
             if self.head.is_none() {
-                self.tail = None;
+                self.tail = std::ptr::null_mut();
             }
 
             head.elem
@@ -50,6 +57,13 @@ impl<'a, T> List<'a, T> {
     }
 }
 
+impl<T> Default for List<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[cfg(test)]
 mod test {
     use super::List;
     #[test]
@@ -78,6 +92,15 @@ mod test {
 
         // Check exhaustion
         assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+
+        // Check the exhaustion case fixed the pointer right
+        list.push(6);
+        list.push(7);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(6));
+        assert_eq!(list.pop(), Some(7));
         assert_eq!(list.pop(), None);
     }
 }
